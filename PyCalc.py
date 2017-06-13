0eaca3127f90059cdef4e565f1f886a2c65cb740
Jan Bures <info@janbures.com>
2017-06-13 03:06:39 -0400

    User input added

---
+++ PyCalc.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+# Terminal based calculator
+#############################
+# Version 0.1
+# 2017.06.13
+
+
+print("insert first number and press ENTER")
+a = input()
+print("inserted", a)
